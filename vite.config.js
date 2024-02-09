b12788c21c9ac1993e8c4a30185d980e84ca9265
ZYevhenii <zinich911@gmail.com>
2024-02-09 11:43:45 +0200

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/todo-list-test",
   plugins: [react()],
   server: {
     port: 4040,
